122db8d40521ec1e62625a2e189fddfe5d4a2f6d
Hugo Terelle <hugoterelle@gmail.com>
2017-05-29 14:07:09 +0200

    removed fody + added button for binding

--- SharedProject1/ViewModels/FirstViewModel.cs
+++ SharedProject1/ViewModels/FirstViewModel.cs
@@ -8,16 +8,65 @@ namespace mvvmcross1.Core.ViewModels
 
 	}
 
-	public class FirstViewModel
-		: BaseViewModel
+	public class FirstViewModel : BaseViewModel
 	{
-		public string Hello { get; set; } = "Hello World";
-		public string Hello2 { get; set; } = "Hello World 2";
+    private string _hello = "Hello World";
+	  public string Hello
+	  {
+	    get => _hello;
+	    set
+	    {
+	      _hello = value;
+        RaisePropertyChanged(() => Hello);
+	    }
+	  }
 
-		public bool IsVisible { get; set; }
-		public bool IsVisible2 { get; set; }
+	  private string _hello2 = "Hello World 2";
+	  public string Hello2
+	  {
+	    get => _hello2;
 
-		public override void Start()
+	    set
+	    {
+	      _hello2 = value;
+        RaisePropertyChanged(() => Hello2);
+	    }
+	  }
+
+	  private string _hello3;
+	  public string Hello3
+	  {
+	    get => _hello3;
+	    set
+	    {
+	      _hello3 = value; 
+	      RaisePropertyChanged(() => Hello3);
+	    }
+	  }
+
+	  private bool _isVisible;
+	  public bool IsVisible
+	  {
+	    get => _isVisible;
+	    set
+	    {
+	      _isVisible = value;
+	      RaisePropertyChanged(() => IsVisible);
+	    }
+	  }
+
+	  private bool _isVisible2;
+	  public bool IsVisible2
+	  {
+	    get => _isVisible2;
+	    set
+	    {
+	      _isVisible2 = value; 
+	      RaisePropertyChanged(() => IsVisible2);
+	    }
+	  }
+
+	  public override void Start()
 		{
 			base.Start();
 			Task.Run(async () =>
@@ -26,18 +75,23 @@ namespace mvvmcross1.Core.ViewModels
 				{
 					await Task.Delay(1000);
 					IsVisible = !IsVisible;
-				}
+				  Hello = IsVisible ? "Test 1" : "Test 2";
+        }
 			});
 		}
 
 		public override async Task Initialize()
 		{
+		  ShouldAlwaysRaiseInpcOnUserInterfaceThread(true);
 			await base.Initialize();
 			for (var i = 0; i < 100; i++)
 			{
 				await Task.Delay(1000);
 				IsVisible2 = !IsVisible2;
+				Hello2 = IsVisible2 ? "Test 1" : "Test 2";
 			}
 		}
-	}
+
+	  public IMvxAsyncCommand SetHello3Command => new MvxAsyncCommand(() => Task.Run(() => Hello3 = Hello));
+  }
 }
